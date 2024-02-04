e751b90d9b908201a830cfe7cc1133ea6722107a
tatenodev <gd28.c.tateno@gmail.com>
2024-02-04 16:03:05 +0900

    add clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +15,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
+		<ClerkProvider>
+			<html lang="ja">
+				<body>{children}</body>
+			</html>
+		</ClerkProvider>
 	);
 }
