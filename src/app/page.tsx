f2ca82e27f27fb631ed55f548093d3fcc4c6c503
tatenodev <gd28.c.tateno@gmail.com>
2024-02-04 18:22:05 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, currentUser } from "@clerk/nextjs";
+import Link from "next/link";
 
-export default function Home() {
+export default async function HomePage() {
+	const user = await currentUser();
+
+	if (!user) {
+		return (
+			<div>
+				<h1>Liebestraume</h1>
+				<Link href="/sign-in">SignIn</Link>
+			</div>
+		);
+	}
+
 	return (
 		<main>
-			<p>home</p>
+			<h1>Liebestraume</h1>
 			<UserButton afterSignOutUrl="/" />
+			<div>{JSON.stringify(user)}</div>
 		</main>
 	);
 }
