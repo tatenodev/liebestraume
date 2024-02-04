3a03dcde1a6c2da5bddb79da71562b7a5949509b
tatenodev <gd28.c.tateno@gmail.com>
2024-02-04 16:13:30 +0900

    add sign-in sign-up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
 	return (
 		<main>
+			<p>home</p>
 			<UserButton afterSignOutUrl="/" />
 		</main>
 	);
--- src/middleware.ts
+++ src/middleware.ts
@@ -6,5 +6,6 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({});
 
 export const config = {
-	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+	matcher: ["/dashboard"],
+	// matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
