223412f98f9bbbfd58405f93a2dec62c8efc880c
tatenodev <gd28.c.tateno@gmail.com>
2024-02-11 23:19:52 +0900

    webhookでuser作成

--- src/app/api/listen-user/route.ts
+++ src/app/api/listen-user/route.ts
@@ -1,15 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 type ClerkWebhookMessage = {
-  data: {
-    id: string;
-  }
+  data: { id: string };
   type: "user.created" | "user.updated" | "user.deleted";
-}
+};
 
 export async function POST(req: Request) {
   const { data, type }: ClerkWebhookMessage = await req.json();
+  const uid = data.id;
 
-  console.log(type);
-  console.log(data);
+  switch (type) {
+    case "user.created":
+      await prisma.user.create({ data: { uid } });
+      break;
+
+    case "user.deleted": {
+      const deleteBooks = prisma.book.deleteMany({ where: { user: { uid } } });
+      const deleteUser = prisma.user.delete({ where: { uid } });
+      prisma.$transaction([deleteBooks, deleteUser]);
+      break;
+    }
+
+    default:
+      break;
+  }
 
   return new Response("Success!", {
     status: 200,
