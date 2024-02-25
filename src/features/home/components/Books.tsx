710b8f482e216dc5fd27214d83f75915bddd808f
tatenodev <gd28.c.tateno@gmail.com>
2024-02-25 17:10:41 +0900

    本の分類

--- src/features/home/components/Books.tsx
+++ src/features/home/components/Books.tsx
@@ -1,26 +1,54 @@
 "use client";
 
 import { View } from "@adobe/react-spectrum";
+import { Item, TabList, TabPanels, Tabs } from "@adobe/react-spectrum";
 import { BookItem } from "./BookItem";
-import {Item, TabList, TabPanels, Tabs} from '@adobe/react-spectrum'
+
+type Book = {
+  id: number;
+  title: string;
+  totalPage: number;
+  currentPage: number;
+  startDate: Date;
+  endDate: Date;
+  createdAt: Date;
+  userId: number;
+};
 
 type BooksProps = {
-  data:
-    | {
-        id: number;
-        title: string;
-        totalPage: number;
-        currentPage: number;
-        startDate: Date;
-        endDate: Date;
-        createdAt: Date;
-        userId: number;
-      }[]
-    | null;
+  data: Book[] | null;
+};
+
+type CategorizeBooks = {
+  newBooks: Book[];
+  inProgress: Book[];
+  done: Book[];
 };
 
 export function Books({ data }: BooksProps) {
   if (!data) return <></>;
+
+  const nowTime = new Date().getTime();
+  const initialBooks: CategorizeBooks = {
+    newBooks: [],
+    inProgress: [],
+    done: [],
+  };
+
+  const categorizeBooks = data.reduce((prev, current) => {
+    if (current.totalPage === current.currentPage) {
+      prev.done.push(current);
+    }
+
+    const startTime = new Date(current.startDate).getTime();
+    if (nowTime > startTime || current.currentPage !== 0) {
+      prev.inProgress.push(current);
+    } else {
+      prev.newBooks.push(current);
+    }
+
+    return prev;
+  }, initialBooks);
 
   return (
     <View paddingX="size-200">
@@ -29,17 +57,31 @@ export function Books({ data }: BooksProps) {
           <Item key="New">これから読む</Item>
           <Item key="InProgress">進行中</Item>
           <Item key="Done">読了</Item>
+          <Item key="All">全て</Item>
         </TabList>
         <TabPanels>
           <Item key="New">
-            {data.map((item) => (
-              <BookItem key={item.id} book={item} />
+            {categorizeBooks.newBooks.map((book) => (
+              <BookItem key={book.id} book={book} />
             ))}
           </Item>
-          <Item key="InProgress">in progress</Item>
-          <Item key="Done">Done</Item>
+          <Item key="InProgress">
+            {categorizeBooks.inProgress.map((book) => (
+              <BookItem key={book.id} book={book} />
+            ))}
+          </Item>
+          <Item key="Done">
+            {categorizeBooks.done.map((book) => (
+              <BookItem key={book.id} book={book} />
+            ))}
+          </Item>
+          <Item key="All">
+            {data.map((book) => (
+              <BookItem key={book.id} book={book} />
+            ))}
+          </Item>
         </TabPanels>
       </Tabs>
-      </View>
+    </View>
   );
 }
